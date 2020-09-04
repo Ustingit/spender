b8ebc0d9a1d37d7047c0a97ce7b2ea13e31f1220
ustinovich.yuryi <ustinhtc@gmail.com>
2020-09-04 22:56:42 +0300

    add src foler

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import RightWrapper, { WrapperType } from './Components/RightWrapper';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      type: WrapperType.HISTORY
+    }
+  }
+
+  render (){
+    return (
+    <Container>
+      <Jumbotron>
+        Spender - app for calculating of your daily spends!
+      </Jumbotron>
+        <Row>
+          <Col md={3} lg={3}>
+              <ButtonGroup vertical>
+                  <Button onClick={ () => { this.setState({ type: WrapperType.HISTORY }) } } >History</Button>
+                  <Button onClick={ () => { this.setState({ type: WrapperType.SPEND_TYPES }) } } >Spend types</Button>
+                  <Button>About</Button>
+              </ButtonGroup>
+          </Col>
+          <Col md={9} lg={9}>
+              <RightWrapper type={this.state.type} />
+          </Col>
+        </Row>
+    </Container>
+    );  
+  }
 }
 
 export default App;
---
+++ src/Components/RightWrapper.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const RightPageType = {
+    HISTORY: "HISTORY",
+    SPEND_TYPES: "SPEND_TYPES",
+    STATISTICS: "STATISTICS",
+    ABOUT: "ABOUT"
+}
+
+class RightWrapper extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            type: RightPageType.HISTORY
+        }
+    }
+
+    renderSwitch(param){
+        switch (param) {
+            case RightPageType.HISTORY:
+                return <div>List of spends:</div>
+            case RightPageType.ABOUT:
+                return <div>ABUT</div>
+            case RightPageType.SPEND_TYPES:
+                return <div>type!</div>
+            default:
+                return <div></div>
+        }
+    }
+    
+    render() {
+        return(
+            <div>
+            {                
+                this.renderSwitch(this.state.type)
+            }
+            </div>
+        );
+    }
+}
+
+export default RightWrapper;
+export { RightPageType as WrapperType }
