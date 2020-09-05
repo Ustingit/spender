3f1786778e024a9a72562094f34aff455b125d15
ustinovich.yuryi <ustinhtc@gmail.com>
2020-09-05 22:56:04 +0300

    add list of types

---
+++ src/Components/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+class About extends React.Component {
+
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col>Spender app. by <a href="https://www.linkedin.com/in/yustinovich/" target="_blank" >Yuryi Ustsinovich</a></Col>
+                    <Col>Created for training some skills related to React library. I' trying to use it for controlling of the my spends. You can try as well!</Col>
+                    <Col>All questions - <a href="mailto:ustinhtc@gmail.com">ustinhtc@gmail.com</a></Col>
+                </Row>
+            </Container>
+        );
+    }
+}
+
+export default About;
--- src/Components/RightWrapper.js
+++ src/Components/RightWrapper.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import About from './About';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import ListGroup from 'react-bootstrap/ListGroup'
 
 const RightPageType = {
     HISTORY: "HISTORY",
     SPEND_TYPES: "SPEND_TYPES",
     STATISTICS: "STATISTICS",
     ABOUT: "ABOUT"
+}
+
+let ExtendedTypes = {
+    HISTORY: { name: "HISTORY", default: true, comment: "список всех расходов и приходов" },
+    SPEND_TYPES: { name: "SPEND_TYPES", default: true, comment: "типы расходов и приходов" },
+    STATISTICS: { name: "STATISTICS", default: true, comment: "статистика по всем расходам и приходам" },
+    ABOUT: { name: "ABOUT", default: true, comment: "о нас" }
 }
 
 class RightWrapper extends React.Component {
@@ -20,9 +32,9 @@ class RightWrapper extends React.Component {
             case RightPageType.HISTORY:
                 return <div>List of spends:</div>
             case RightPageType.ABOUT:
-                return <div>ABUT</div>
+                return <About />
             case RightPageType.SPEND_TYPES:
-                return <div>type!</div>
+                return <SpendTypes />
             default:
                 return <div></div>
         }
@@ -35,6 +47,49 @@ class RightWrapper extends React.Component {
                 this.renderSwitch(this.props.type)
             }
             </div>
+        );
+    }
+}
+
+function notPossibleToEditDefaultType() {
+    alert("Стандартные типы редактировать запрещено!");
+}
+
+function editType() {
+    alert("2!");
+}
+
+class SpendTypes extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <Container>
+                <Row>
+                    <Col md={12} lg={12} >
+                        Currenly available types:
+                    </Col>
+                    <Col md={12} lg={12} >
+                        <ListGroup>
+                            {
+                            Object.keys(ExtendedTypes).map(function(key, index) { 
+                                return <ListGroup.Item action onClick={ () => {
+                                    if (ExtendedTypes[key].default) {
+                                      notPossibleToEditDefaultType()
+                                    } else {
+                                      editType()
+                                    }
+                                  } } >
+                                        {ExtendedTypes[key].name}
+                                </ListGroup.Item>
+                            })
+                        }
+                        </ListGroup>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
