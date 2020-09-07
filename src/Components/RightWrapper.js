fb08fd26109b10922e86cfbf470349b2af03afdd
ustinovich.yuryi <ustinhtc@gmail.com>
2020-09-07 21:52:33 +0300

    rewrited modal to class component

--- src/Components/RightWrapper.js
+++ src/Components/RightWrapper.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import About from './About';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import ListGroup from 'react-bootstrap/ListGroup'
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
-import Tooltip from 'react-bootstrap/Tooltip'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import Tooltip from 'react-bootstrap/Tooltip';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+const HIGH_LEVEL_TYPE_INCOME = 'INCOME';
+const HIGH_LEVEL_TYPE_COSTS = 'COSTS';
 
 const RightPageType = {
     HISTORY: "HISTORY",
@@ -17,17 +22,26 @@ const RightPageType = {
     ABOUT: "ABOUT"
 }
 
+const Words = {
+    'INCOME': {
+        tooltipText: "прихода"
+    },
+    'COSTS': {
+        tooltipText: "расходов"
+    }
+}
+
 let IncomeOrCostTypes = {
     INCOME: {
-        "зарплата": { id: 0, name: "зарплата", default: true, comment: "сюда входит аванс, зарплата, премии или бонусы, которые вы получили от основного или дополнительных видов деятельности" },
-        "долг": { id: 1, name: "долг", default: true, comment: "возврат отдолженных средств" },
-        "пассивный доход": { id: 2, name: "пассивный доход", default: true, comment: "доходы, полученные от инвестиций, акци и т.д." },
-        "подарок": { id: 3, name: "подарок", default: true, comment: "средства, подаренные по какому-либо поводу." }
+        "зарплата": { id: 0, name: "зарплата", default: true, comment: "сюда входит аванс, зарплата, премии или бонусы, которые вы получили от основного или дополнительных видов деятельности", type: HIGH_LEVEL_TYPE_INCOME },
+        "долг": { id: 1, name: "долг", default: true, comment: "возврат отдолженных средств", type: HIGH_LEVEL_TYPE_INCOME },
+        "пассивный доход": { id: 2, name: "пассивный доход", default: true, comment: "доходы, полученные от депозитов, инвестиций, и т.д.", type: HIGH_LEVEL_TYPE_INCOME },
+        "подарок": { id: 3, name: "подарок", default: true, comment: "средства, подаренные по какому-либо поводу.", type: HIGH_LEVEL_TYPE_INCOME }
     },
     COSTS: {
-        "питание": { id: 0, name: "питание", default: true, comment: "расходы на питание." },
-        "хоз. расходы": { id: 1, name: "хоз. расходы", default: true, comment: "расходы на хозяйственные нужды." },
-        "транспорт": { id: 2, name: "транспорт", default: true, comment: "расходы на транспорт, такси либо шэринг." }
+        "питание": { id: 0, name: "питание", default: true, comment: "расходы на питание.", type: HIGH_LEVEL_TYPE_COSTS },
+        "хоз. расходы": { id: 1, name: "хоз. расходы", default: true, comment: "расходы на хозяйственные нужды.", type: HIGH_LEVEL_TYPE_COSTS },
+        "транспорт": { id: 2, name: "транспорт", default: true, comment: "расходы на транспорт, такси либо шэринг.", type: HIGH_LEVEL_TYPE_COSTS }
     }
 }
 
@@ -74,6 +88,9 @@ function editType() {
 class SpendTypes extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            showAddModal: false
+        }
     }
 
     render() {
@@ -86,13 +103,7 @@ class SpendTypes extends React.Component {
                     <Col md={12} lg={12} >
                         <ListGroup>
                             <h3>Приходы:&nbsp; 
-                                <OverlayTrigger placement="right" overlay={
-                                                                      <Tooltip id={`tooltip-right`}>
-                                                                            Добавить тип прихода
-                                                                      </Tooltip>
-                                                                    } >
-                                    <a><FontAwesomeIcon icon={faPlusCircle} /></a>
-                                </OverlayTrigger>
+                            <AddTypeModalWindowClass spendType={HIGH_LEVEL_TYPE_INCOME} />
                                 </h3><br />                                
                             {
                                 Object.keys(IncomeOrCostTypes.INCOME).map(function(key, index) { 
@@ -136,6 +147,94 @@ class SpendTypes extends React.Component {
         );
     }
 }
+
+function AddTypeModalWindow() {
+    const [show, setShow] = useState(false);
+    const [spendType, changeSpendType] = useState(HIGH_LEVEL_TYPE_INCOME);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+      <>
+        <OverlayTrigger placement="right" overlay={
+                <Tooltip id={`tooltip-right`}>
+                      Добавить тип {Words[spendType].tooltipText}
+                </Tooltip>    
+            } >
+                <a onClick={ handleShow } ><FontAwesomeIcon icon={faPlusCircle} /></a>
+        </OverlayTrigger>
+  
+        <Modal show={show} onHide={handleClose} animation={false}>
+          <Modal.Header closeButton>
+            <Modal.Title><h3>Добавление типа {Words[spendType].tooltipText}</h3></Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+                <div>Добавьте имя нового типа:</div>
+                <div><input type="text" id="newTypeName" placeholder="введите название"></input></div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+                Закрыть
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+                Добавить
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+
+class AddTypeModalWindowClass extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            spendType: props.spendType ?? HIGH_LEVEL_TYPE_INCOME,
+            showModal: false
+        }
+    }
+
+    handleShow() {
+        this.setState({ showModal: true });
+    }
+
+    handleClose() {
+        this.setState({ showModal: false });
+    }
+
+    render() {
+    return (
+      <>
+        <OverlayTrigger placement="right" overlay={
+                <Tooltip id={`tooltip-right`}>
+                      Добавить тип {Words[this.state.spendType].tooltipText}
+                </Tooltip>    
+            } >
+                <a onClick={ () => this.handleShow() } ><FontAwesomeIcon icon={faPlusCircle} /></a>
+        </OverlayTrigger>
+  
+        <Modal show={this.state.showModal} onHide={() => this.handleClose()} animation={false}>
+          <Modal.Header closeButton>
+            <Modal.Title><h3>Добавление типа {Words[this.state.spendType].tooltipText}</h3></Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+                <div>Добавьте имя нового типа:</div>
+                <div><input type="text" id="newTypeName" placeholder="введите название"></input></div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => this.handleClose()}>
+                Закрыть
+            </Button>
+            <Button variant="primary" onClick={() => this.handleClose()}>
+                Добавить
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );}
+}
+
 
 export default RightWrapper;
 export { RightPageType as WrapperType }
