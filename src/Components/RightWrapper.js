007e83720f155bcd762fe45bc74e9b932f6fed82
ustinovich.yuryi <ustinhtc@gmail.com>
2020-09-13 15:10:20 +0300

    move modal to concrete component

--- src/Components/RightWrapper.js
+++ src/Components/RightWrapper.js
@@ -4,30 +4,16 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
-import Tooltip from 'react-bootstrap/Tooltip';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
 import { HIGH_LEVEL_TYPE_INCOME, HIGH_LEVEL_TYPE_COSTS, incomeTypes, costTypes } from '../Data/Types';
 import { EditAndDeleteItemControl } from './ItemControls';
+import AddTypeModalWindowClass from './AddTypeModalWindowClass';
 
 const RightPageType = {
     HISTORY: "HISTORY",
     SPEND_TYPES: "SPEND_TYPES",
     STATISTICS: "STATISTICS",
     ABOUT: "ABOUT"
-}
-
-const Words = {
-    'INCOME': {
-        tooltipText: "прихода"
-    },
-    'COSTS': {
-        tooltipText: "расходов"
-    }
 }
 
 class RightWrapper extends React.Component {
@@ -142,7 +128,7 @@ class SpendTypes extends React.Component {
                                     this.state.costsTypes.map(typeObjectInformation => { 
                                         return <ListGroup.Item key={typeObjectInformation.id} action 
                                                     onClick={ () => this.handleClick(typeObjectInformation.default) } >
-                                                {typeObjectInformation.name}
+                                                <div>{typeObjectInformation.name}  {!typeObjectInformation.default && <EditAndDeleteItemControl type={HIGH_LEVEL_TYPE_INCOME} objectId={typeObjectInformation.id} /> }</div>
                                         </ListGroup.Item>
                                 })
                             }
@@ -153,77 +139,6 @@ class SpendTypes extends React.Component {
         );
     }
 }
-
-class AddTypeModalWindowClass extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            spendType: props.spendType ?? HIGH_LEVEL_TYPE_INCOME,
-            showModal: false,
-            newTypeName: "",
-            newComment: ""
-        }
-    }
-
-    handleShow() {
-        this.setState({ showModal: true });
-    }
-
-    handleClose() {
-        this.setState({ showModal: false });
-    }
-
-    onNameChange = (e) => {
-        this.setState({ newTypeName: e.target.value })
-    }
-
-    onCommentChange = (e) => {
-        this.setState({ newComment: e.target.value })
-    }
-
-    handleSave(event){
-        var test = event.target.value;
-        console.log(test);
-
-        this.handleClose();
-        
-        if (this.props.setNewType) {
-            this.props.setNewType(this.state.spendType, this.state.newTypeName, this.state.newComment !== "" ?  this.state.newComment : this.state.newTypeName );
-        }
-    }
-
-    render() {
-    return (
-      <>
-        <OverlayTrigger placement="right" overlay={
-                <Tooltip id={`tooltip-right`}>
-                      Добавить тип {Words[this.state.spendType].tooltipText}
-                </Tooltip>    
-            } >
-                <a onClick={ () => this.handleShow() } ><FontAwesomeIcon icon={faPlusCircle} /></a>
-        </OverlayTrigger>
-  
-        <Modal show={this.state.showModal} onHide={() => this.handleClose()} animation={false}>
-          <Modal.Header>
-            <Modal.Title><h3>Добавление типа {Words[this.state.spendType].tooltipText}</h3></Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-                <input type="text" placeholder="Впишите название нового типа" onChange={ this.onNameChange } /><br />
-                <input type="text" placeholder="Впишите комментарий" onChange={ this.onCommentChange } />
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={() => this.handleClose()}>
-                Закрыть
-            </Button>
-            <Button variant="primary" onClick={ this.handleSave.bind(this) }>
-                Добавить
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );}
-}
-
 
 export default RightWrapper;
 export { RightPageType as WrapperType }
